ac3275ceca75d1580d5f9229d8d92ef9875e3419
YumaTokuhashi <1021kke@gmail.com>
2016-07-18 14:59:55 +0900

    Update kadai1.m

--- kadai1.m
+++ kadai1.m
@@ -7,7 +7,7 @@ imagesc(ORG); axis image; % 画像の表示
 
 によって，原画像を読み込み，表示した結果を図１に示す．
 
-![原画像](https://github.com/mackhasegawa/lecture_image_processing/blob/master/image/org_img.png?raw=true)  
+![原画像](https://raw.githubusercontent.com/YumaTokuhashi/lecture_image_processing/master/1-1.bmp)  
 図1 原画像
 
 原画像を1/2サンプリングするには，画像を1/2倍に縮小した後，2倍に拡大すればよい．なお，拡大する際には，単純補間するために「box」オプションを設定する．
@@ -17,7 +17,7 @@ IMG2 = imresize(IMG,2,'box'); % 画像の拡大
 
 1/2サンプリングの結果を図２に示す．
 
-![原画像](https://github.com/mackhasegawa/lecture_image_processing/blob/master/image/kadai1_1.png?raw=true)  
+![原画像](https://raw.githubusercontent.com/YumaTokuhashi/lecture_image_processing/master/1-2.bmp)  
 図2 1/2サンプリング
 
 同様に原画像を1/4サンプリングするには，画像を1/2倍に縮小した後，2倍に拡大すればよい．すなわち，
@@ -27,7 +27,7 @@ IMG2 = imresize(IMG,2,'box'); % 画像の拡大
 
 とする．1/4サンプリングの結果を図３に示す．
 
-![原画像](https://github.com/mackhasegawa/lecture_image_processing/blob/master/image/kadai1_2.png?raw=true)  
+![原画像](https://raw.githubusercontent.com/YumaTokuhashi/lecture_image_processing/master/1-3.bmp)  
 図3 1/4サンプリング
 
 1/8から1/32サンプリングは，
@@ -37,47 +37,15 @@ IMG2 = imresize(IMG,2,'box'); % 画像の拡大
 
 を繰り返す．サンプリングの結果を図４～６に示す．
 
-![原画像](https://github.com/mackhasegawa/lecture_image_processing/blob/master/image/kadai1_3.png?raw=true)  
+![原画像](https://raw.githubusercontent.com/YumaTokuhashi/lecture_image_processing/master/1-4.bmp)  
 図4 1/8サンプリング
 
-![原画像](https://github.com/mackhasegawa/lecture_image_processing/blob/master/image/kadai1_4.png?raw=true)  
+![原画像](https://raw.githubusercontent.com/YumaTokuhashi/lecture_image_processing/master/1-5.bmp)  
 図5 1/16サンプリング
 
-![原画像](https://github.com/mackhasegawa/lecture_image_processing/blob/master/image/kadai1_5.png?raw=true)  
+![原画像](https://raw.githubusercontent.com/YumaTokuhashi/lecture_image_processing/master/1-6.bmp)  
 図6 1/32サンプリング
 
 このようにサンプリング幅が大きくなると，モザイク状のサンプリング歪みが発生する．
 
 
-．
-% 課題作成にあたっては「Lenna」以外の画像を用いよ．
-
-clear; % 変数のオールクリア
-
-ORG=imread('Lenna.png'); % 原画像の入力
-imagesc(ORG); axis image; % 画像の表示
-pause; % 一時停止
-
-IMG = imresize(ORG,0.5); % 画像の縮小
-IMG2 = imresize(IMG,2,'box'); % 画像の拡大
-imagesc(IMG2); axis image; % 画像の表示
-pause; % 一時停止
-
-IMG = imresize(IMG,0.5); % 画像の縮小
-IMG2 = imresize(IMG,4,'box'); % 画像の拡大
-imagesc(IMG2); axis image; % 画像の表示
-pause; % 一時停止
-
-IMG = imresize(IMG,0.5); % 画像の縮小
-IMG2 = imresize(IMG,8,'box'); % 画像の拡大
-imagesc(IMG2); axis image; % 画像の表示
-pause; % 一時停止
-
-IMG = imresize(IMG,0.5); % 画像の縮小
-IMG2 = imresize(IMG,16,'box'); % 画像の拡大
-imagesc(IMG2); axis image; % 画像の表示
-pause; % 一時停止
-
-IMG = imresize(IMG,0.5); % 画像の縮小
-IMG2 = imresize(IMG,32,'box'); % 画像の拡大
-imagesc(IMG2); axis image; % 画像の表示
